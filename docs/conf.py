91741f678fbad891410704c48566cdc456352b43
Aditya Sonpal <adityaso@buffalo.edu>
2019-10-30 16:25:19 -0400

    documentation

--- docs/conf.py
+++ docs/conf.py
@@ -135,8 +135,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ChemLG.tex', 'ChemLG Documentation',
-     'Gaurav', 'manual'),
+    (master_doc, 'ChemBDDB.tex', 'ChemBDDB Documentation',
+     'Aditya', 'manual'),
 ]
 
 
